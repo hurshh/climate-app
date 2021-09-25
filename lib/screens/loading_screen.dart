ae8139245bbd501dbb2808f31cfdc8e39a1923b4
hurshh <quasarhrsh@gmail.com>
2021-09-25 12:36:26 +0530

    location api implemented

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +9,20 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+
+  Future<void> getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: () {
+          color: Colors.red,
+          onPressed: (){
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
